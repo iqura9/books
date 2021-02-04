e8a838c21e4cf26f50750bb9a401bbd8ef667ff2
iqura9 <77846496+iqura9@users.noreply.github.com>
2021-02-04 18:57:50 +0200

    1

--- together.js
+++ together.js
@@ -13,48 +13,88 @@ function loadGoods() {
     for (var key in data) {
       if (data[key]["filter"] == "done2020") {
         out+='<div class="box">';
+        out+='	<div class="newWrapper">';
         out+='<div class="FlexCorToRow">';
-        out+='<div class="smallIconLeft">';
-        out+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>'
-        out+=  '</div>';
-        out +='<div class="smallIconMid">';
-        if(data[key]["bookOrAudio"]==="Book")
-          out +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
-        else
-          out +='<div class="box_smallIconMid_audio">'+data[key]["bookOrAudio"]+'</div>';
-
-            out +='</div>';
-            out +='<div class="smallIconRight">';
-            out += '<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
-          out +='</div>';
-
-        out +='</div>';
-        
+                                  
         out +='<img src="'+ data[key]["img"] +'" alt="">';
         out +='<p  class="title"><span>' + data[key]["name"] + '</span><br> '+ data[key]["autor"] +'</p>';
-        out +='</div>';;
+        out +='</div>';
+        out +='</div>';
+                                
+        out +='<div class="lowSkill">';
+        out+='<div class="smallIconLeft">';
+                                    
+        out+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>';
+        out+= '<div class="box_smallIconLeft1">'+data[key]["language"]+'</div>';
+        out+= '<div class="whyThis">Мова</div>'
+                                  
+        out +='</div>';
+        out +='<div class="smallIconMid">';
+        if(data[key]["bookOrAudio"]==="Book"){
+          out +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
+          out +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+        }       
+        else{
+          out +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';	
+          out +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+        }
+        
+        out+= '<div class="whyThis">Формат</div>';
+        out +='</div>';
+        
+        out +='<div class="smallIconRight">';
+        out +='<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+        out +='<div class="box_smallIconRight1">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+        if(data[key]["bookOrAudio"]==="Book")
+        out+= '<div class="whyThis">Сторінка</div>';
+        else
+        out+= '<div class="whyThis">Прослухано</div>';
+        out +='</div>';
+                                  
+        out +='</div>';
+        out +='</div>';
       }
       if (data[key]["filter"] == "done2021") {
         out1+='<div class="box">';
+        out1+='	<div class="newWrapper">';
         out1+='<div class="FlexCorToRow">';
-        out1+='<div class="smallIconLeft">';
-        out1+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>'
-        out1+=  '</div>';
-        out1 +='<div class="smallIconMid">';
-        if(data[key]["bookOrAudio"]==="Book")
-          out1 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
-        else
-          out1 +='<div class="box_smallIconMid_audio">'+data[key]["bookOrAudio"]+'</div>';
-
-            out1 +='</div>';
-            out1 +='<div class="smallIconRight">';
-            out1 += '<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
-          out1 +='</div>';
-
-        out1 +='</div>';
-        
+                                  
         out1 +='<img src="'+ data[key]["img"] +'" alt="">';
         out1 +='<p  class="title"><span>' + data[key]["name"] + '</span><br> '+ data[key]["autor"] +'</p>';
+        out1 +='</div>';
+        out1 +='</div>';
+                                
+        out1 +='<div class="lowSkill">';
+        out1+='<div class="smallIconLeft">';
+                                    
+        out1+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>';
+        out1+= '<div class="box_smallIconLeft1">'+data[key]["language"]+'</div>';
+        out1+= '<div class="whyThis">Мова</div>'
+                                  
+        out1 +='</div>';
+        out1 +='<div class="smallIconMid">';
+        if(data[key]["bookOrAudio"]==="Book"){
+          out1 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
+          out1 +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+        }       
+        else{
+          out1 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';	
+          out1 +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+        }
+        
+        out1+= '<div class="whyThis">Формат</div>';
+        out1 +='</div>';
+        
+        out1 +='<div class="smallIconRight">';
+        out1 +='<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+        out1 +='<div class="box_smallIconRight1">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+        if(data[key]["bookOrAudio"]==="Book")
+        out1+= '<div class="whyThis">Сторінка</div>';
+        else
+        out1+= '<div class="whyThis">Прослухано</div>';
+        out1 +='</div>';
+                                  
+        out1 +='</div>';
         out1 +='</div>';
       }
       if (data[key]["filter"] == "reading") {
@@ -62,54 +102,95 @@ function loadGoods() {
   
     
         out2+='<div class="box">';
-        out2+='<div class="FlexCorToRow">';
-        out2+='<div class="smallIconLeft">';
-        out2+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>'
-        out2+=  '</div>';
-        out2 +='<div class="smallIconMid">';
-        
-        if(data[key]["bookOrAudio"]==="Book")
-          out2 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
+out2+='	<div class="newWrapper">';
+out2+='<div class="FlexCorToRow">';
+													
+out2 +='<img src="'+ data[key]["img"] +'" alt="">';
+out2 +='<p  class="title"><span>' + data[key]["name"] + '</span><br> '+ data[key]["autor"] +'</p>';
+out2 +='</div>';
+out2 +='</div>';
+												
+out2 +='<div class="lowSkill">';
+out2+='<div class="smallIconLeft">';
+														
+out2+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>';
+out2+= '<div class="box_smallIconLeft1">'+data[key]["language"]+'</div>';
+out2+= '<div class="whyThis">Мова</div>'
+													
+out2 +='</div>';
+out2 +='<div class="smallIconMid">';
+if(data[key]["bookOrAudio"]==="Book"){
+  out2 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
+  out2 +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+}       
+else{
+  out2 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';	
+  out2 +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+}
+
+out2+= '<div class="whyThis">Формат</div>';
+out2 +='</div>';
+
+out2 +='<div class="smallIconRight">';
+out2 +='<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+out2 +='<div class="box_smallIconRight1">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+if(data[key]["bookOrAudio"]==="Book")
+        out2+= '<div class="whyThis">Сторінка</div>';
         else
-          out2 +='<div class="box_smallIconMid_audio">'+data[key]["bookOrAudio"]+'</div>';
-
-            out2 +='</div>';
-            out2 +='<div class="smallIconRight">';
-            out2 += '<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
-          out2 +='</div>';
-
-        out2 +='</div>';
-        
-        out2 +='<img src="'+ data[key]["img"] +'" alt="">';
-        out2 +='<p  class="title"><span>' + data[key]["name"] + '</span><br> '+ data[key]["autor"] +'</p>';
-        out2 +='</div>';
+        out2+= '<div class="whyThis">Прослухано</div>';
+out2 +='</div>';
+													
+out2 +='</div>';
+out2 +='</div>';
 
 
 
 
       }
       if (data[key]["filter"] == "toread") {
-        out3+='<div class="box">';
-        out3+='<div class="FlexCorToRow">';
-        out3+='<div class="smallIconLeft">';
-        out3+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>'
-        out3+=  '</div>';
-        out3 +='<div class="smallIconMid">';
-        if(data[key]["bookOrAudio"]==="Book")
-          out3 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
-        else
-          out3 +='<div class="box_smallIconMid_audio">'+data[key]["bookOrAudio"]+'</div>';
-          
-            out3 +='</div>';
-            out3 +='<div class="smallIconRight">';
-            out3 +='<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
-          out3 +='</div>';
 
-        out3 +='</div>';
-        
-        out3 +='<img src="'+ data[key]["img"] +'" alt="">';
-        out3 +='<p  class="title"><span>' + data[key]["name"] + '</span><br> '+ data[key]["autor"] +'</p>';
-        out3 +='</div>';
+        out3+='<div class="box">';
+out3+='	<div class="newWrapper">';
+out3+='<div class="FlexCorToRow">';
+													
+out3 +='<img src="'+ data[key]["img"] +'" alt="">';
+out3 +='<p  class="title"><span>' + data[key]["name"] + '</span><br> '+ data[key]["autor"] +'</p>';
+out3 +='</div>';
+out3 +='</div>';
+												
+out3 +='<div class="lowSkill">';
+out3+='<div class="smallIconLeft">';
+														
+out3+= '<div class="box_smallIconLeft">'+data[key]["language"]+'</div>';
+out3+= '<div class="box_smallIconLeft1">'+data[key]["language"]+'</div>';
+out3+= '<div class="whyThis">Мова</div>'
+													
+out3 +='</div>';
+out3 +='<div class="smallIconMid">';
+if(data[key]["bookOrAudio"]==="Book"){
+  out3 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';
+  out3 +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+}       
+else{
+  out3 +='<div class="box_smallIconMid">'+data[key]["bookOrAudio"]+'</div>';	
+  out3 +='<div class="box_smallIconMid1">'+data[key]["bookOrAudio"]+'</div>';
+}
+
+out3+= '<div class="whyThis">Формат</div>';
+out3 +='</div>';
+
+out3 +='<div class="smallIconRight">';
+out3 +='<div class="box_smallIconRight">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+out3 +='<div class="box_smallIconRight1">'+data[key]["rightNowOnPage"]+'/'+data[key]["Pages"]+'</div>';
+if(data[key]["bookOrAudio"]==="Book")
+        out3+= '<div class="whyThis">Сторінка</div>';
+        else
+        out3+= '<div class="whyThis">Прослухано</div>';
+out3 +='</div>';
+													
+out3 +='</div>';
+out3 +='</div>';
+     
       }
     }
     $("#res").html(out);
